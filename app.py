4de13d528126f3398db55a8d165fb103f9f93192
Plonsky15 <adakouk@gmail.com>
2025-01-03 14:44:56 +0100

    lol

---
+++ app.py
@@ -0,0 +1,122 @@
+import os
+import pandas as pd
+import streamlit as st
+from langchain.agents.agent_toolkits import create_pandas_dataframe_agent
+from langchain.agents.agent_types import AgentType
+from langchain.utilities import PythonREPL
+from langchain_openai import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from dotenv import load_dotenv
+from system_message import messages
+
+load_dotenv()
+
+# Streamlit web app configuration
+st.set_page_config(
+	page_title="Labelvie chatbot",
+	page_icon="💬",
+	layout="wide"
+)
+
+# Streamlit page title
+st.markdown("<h1 style='text-align: center; color: #1e1e1e';></h1>", unsafe_allow_html=True)
+
+# Set the OpenAI API key
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+
+
+# Function to read the uploaded file into a DataFrame
+def read_data(file):
+	if file.name.endswith(".csv"):
+		return pd.read_csv(file, low_memory=False)
+	else:
+		return pd.read_excel(file)
+
+
+# Initialize chat history in Streamlit session state
+if "chat_history" not in st.session_state:
+	st.session_state.chat_history = []
+
+# Initialize DataFrame in session state
+if "df" not in st.session_state:
+	st.session_state.df = None
+
+# Initialize memory for conversation history
+if "memory" not in st.session_state:
+	st.session_state.memory = ConversationBufferMemory()
+
+# # Image logo
+# base_dir = os.path.dirname(__file__)
+# image_path = os.path.join(base_dir, "moha-removebg-preview.png")
+# st.sidebar.image(image_path, width=250)
+
+# File upload widget
+uploaded_file = st.sidebar.file_uploader("Choose a file", type=["csv", "xlsx", "xls"])
+
+if uploaded_file:
+	st.session_state.df = read_data(uploaded_file)
+	st.write("DataFrame Preview:")
+	st.dataframe(st.session_state.df.head())
+	st.success("File uploaded successfully")
+
+# Display chat history
+for message in st.session_state.chat_history:
+	with st.chat_message(message["role"]):
+		if "content" in message:
+			st.markdown(message["content"])
+		elif "plot" in message:
+			st.markdown(message["plot"])
+
+# Input field for user's message
+user_prompt = st.chat_input("Your message")
+
+if user_prompt:
+	# Add user's message to chat history and display it
+	st.chat_message("user").markdown(user_prompt)
+	st.session_state.chat_history.append({"role": "user", "content": user_prompt})
+
+	# Initialize the general LLM
+	general_llm = ChatOpenAI(model="gpt-4o-mini", temperature=0.7, api_key=os.environ["OPENAI_API_KEY"],
+							 max_tokens=1500)
+
+	# Prepare messages for the agent
+	messages = messages
+
+	def respond_to_user():
+		if st.session_state.df is not None:
+
+			# Initialize the LLM for data
+			llm = ChatOpenAI(model="gpt-4o-mini", temperature=0, api_key=os.environ["OPENAI_API_KEY"], max_tokens=1500)
+
+			# Create pandas agent
+			pandas_df_agent = create_pandas_dataframe_agent(
+				llm,
+				st.session_state.df,
+				agent_type=AgentType.OPENAI_FUNCTIONS,
+				memory=st.session_state.memory,
+				tools=[PythonREPL()],
+				verbose=True,
+				return_intermediate_steps=True,
+				allow_dangerous_code=True,
+				max_iterations=30,
+				max_time=60,
+				handle_parsing_errors=True
+			)
+
+			# Prepare messages for the agent and get response
+			messages.append({"role": "user", "content": user_prompt})
+			response = pandas_df_agent.invoke(messages)
+
+		else:
+			messages.append({"role": "user", "content": user_prompt})
+			response = general_llm(messages)
+			return response.content
+
+
+	# Get response from the appropriate agent
+	assistant_response = respond_to_user()
+
+	# Add the assistant's response to chat history and display it
+	st.session_state.chat_history.append({"role": "assistant", "content": assistant_response})
+	with st.chat_message("assistant"):
+		st.markdown(assistant_response)
---
+++ system_message.py
@@ -0,0 +1,57 @@
+messages = [{
+	"role": "system",
+	"content": """
+	    You are a versatile assistant capable of handling general queries and performing data analysis when a DataFrame is provided.
+	    your primary role will be to analyse financial data that will be provided to you.
+	    the data is relative to the sales extraction of labelvie's ecommerce business.
+
+	    **General Queries**:
+	    - Respond as a helpful and knowledgeable assistant.
+
+	    **Data Analysis Queries**:
+	    ### Step 1: Understand the Data
+    	- **Column Descriptions**: Familiarize yourself with the columns in the DataFrame. Here are the descriptions of the columns:
+
+			-'REGION': Represents the geographical region associated with the data.
+			-'BU': Describes the type of store. There are four store types:		
+				Hyper: Hypermarkets (large-scale stores).	
+				Super (also called Market): Supermarkets (medium-scale stores).		
+				Express: Small convenience stores.		
+				Supeco: Discount supermarkets.		
+			-'Département': Refers to a specific section of the store dedicated to a category of products.		
+			-'CLIENT': Describes the sales channel where revenue is generated. Carrefour's ecommerce is primarily conducted through:	
+				Glovo: A third-party application.	
+				Bringo: Carrefour's own delivery app.	
+			-'Rayon': Represents the store aisle where sales are made.
+			-'Nom magasin': Represents the name of the store.
+			-'CA TTC REA': Represents the total revenue (chiffre d'affaires) including VAT (TVA).
+			-'CA HT REA': Represents the total revenue (chiffre d'affaires) excluding VAT (TVA).		
+			-'MB REALISE E': Represents the gross margin, which is the difference between revenue and the cost of goods sold (COGS), expressed as an absolute value.		
+			-'CA TTC HIST': Represents the total revenue (chiffre d'affaires) including VAT for the previous year (2023).		
+			-'CA HT HIST': Represents the total revenue (chiffre d'affaires) excluding VAT for the previous year (2023).	
+			-'MB HIST': Represents the gross margin for the previous year (2023).
+
+
+	    ### For Calculations:
+	    1. **Confirm Data Selection**:
+	       - Verify the correct columns and data range.
+	       - If there’s ambiguity, confirm column names and calculation targets with the user.
+
+	    2. **Break Down Calculations**:
+	       - For multi-step calculations, break down each step, perform it, and show interim results.
+
+	    3. **Ensure Consistency**:
+	       - Re-evaluate or re-run critical steps to confirm accuracy before finalizing results.
+
+	    4. **Show Transparency**:
+	       - Display the calculation formula and any data transformations applied.
+
+	    5. **Handle Limitations**:
+	       - If a calculation is unclear or beyond your scope, ask clarifying questions or inform the user.
+
+	    ### General Guidelines:
+	    - If parameters are unclear, politely ask for clarification.
+	    - If the query contains unknown terms, suggest alternatives and ask for confirmation.
+	    - For short or vague queries, summarize your understanding and confirm with the user before proceeding.
+	  """
+}]
